83985da8425677d362832b991459e788d8f6b11d
Colby Weil <2ndtwo@protonmail.com>
2020-04-16 09:15:42 -0700

    feat(app.vue): add logic for touchability
    Create function that returns "Yes" or "No" depending on whether or not an object is touchable. An
    object's touchability is based on its lifetime property, which represents COVID-19's viability for
    that object.

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,29 @@
 
     <label class="field field__label">
       What do you want to touch?
-      <select class="field__input field__select">
-        <option name="metal">Metal</option>
+      <select
+        class="field__input field__select"
+        v-model="rawSelectedObject"
+        required
+      >
+        <option></option>
+        <option>Metal</option>
       </select>
     </label>
 
     <label class="field field__label">
       How recently could someone have touched this?
-      <input class="field field__input" type="datetime-local" />
+      <input
+        class="field__input"
+        type="datetime-local"
+        v-model="touchedTime"
+        required
+      />
     </label>
+
+    <div v-if="showResult">
+      <p class="result">{{ canTheyTouchThis }}</p>
+    </div>
   </div>
 </template>
 
@@ -26,12 +40,40 @@ export default {
   name: "App",
   data() {
     return {
+      rawSelectedObject: "",
+      touchedTime: null,
       touchableObjects: {
         metal: {
           lifetime: moment.duration(5, "days")
         }
       }
     };
+  },
+  computed: {
+    showResult() {
+      return this.selectedObject && this.touchedTime;
+    },
+    selectedObject() {
+      return this.rawSelectedObject.toLowerCase();
+    },
+    canTheyTouchThis() {
+      if (
+        !(this.selectedObject in this.touchableObjects) ||
+        !this.touchedTime
+      ) {
+        return "";
+      }
+
+      const selectedObjectLifetime = this.touchableObjects[this.selectedObject]
+        .lifetime;
+      const touchedTime = moment(this.touchedTime);
+      const durationDiff = moment.duration(moment().diff(touchedTime));
+      if (durationDiff.asHours() > selectedObjectLifetime.asHours()) {
+        return "Yes";
+      } else {
+        return "No";
+      }
+    }
   }
 };
 </script>
@@ -44,10 +86,24 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+
 h1 {
   margin-bottom: 0.25em;
 }
 .sub-heading {
-  margin: 0 0 1em;
+  font-size: 0.875rem;
+  margin: 0 0 2em;
+}
+
+.field {
+  display: block;
+  text-align: center;
+  margin: 1.25em;
+
+  &__input {
+    display: block;
+    margin: 0.5em auto;
+    text-align: center;
+  }
 }
 </style>
